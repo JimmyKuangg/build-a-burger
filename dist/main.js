f01e647392c4c487be6bebb753e3836d5d140308
Jimmy Kuang <jimmy.kuang96@gmail.com>
2021-09-03 10:17:54 -0700

    Added basic shapes for griddle and condiment table

--- dist/main.js
+++ dist/main.js
@@ -5,30 +5,63 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xfffff);
         
 //CAMERA
-const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, 1000 / 700, 0.1, 1000);
 camera.position.z = 25;
         
 //RENDERER
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(innerWidth, innerHeight);
+renderer.setSize(1000, 700);
 renderer.setPixelRatio(devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
 //TEXTURE LOADER
 const textureLoader = new THREE.TextureLoader();    
         
-//PLANE
+//FLOOR
 //TEXTURE
-const planeTexture = textureLoader.load('./img/floor_tile.jpg');
-planeTexture.wrapS = THREE.RepeatWrapping;
-planeTexture.wrapT = THREE.RepeatWrapping;
-planeTexture.repeat.set( 10, 10 );
+const floorTexture = textureLoader.load('./img/floor_tile.jpg');
+floorTexture.wrapS = THREE.RepeatWrapping;
+floorTexture.wrapT = THREE.RepeatWrapping;
+floorTexture.repeat.set( 10, 10 );
 //SHAPE AND MESH
-const planeGeo = new THREE.PlaneGeometry(110, 50);
-const planeMaterial = new THREE.MeshBasicMaterial({map: planeTexture});
-const plane = new THREE.Mesh(planeGeo, planeMaterial);
-plane.rotation.x = -1;
-scene.add(plane);
+const floorGeo = new THREE.PlaneGeometry(110, 50);
+const floorMaterial = new THREE.MeshBasicMaterial({map: floorTexture});
+const floor = new THREE.Mesh(floorGeo, floorMaterial);
+floor.rotation.x = -1;
+scene.add(floor);
+
+//HOT PLATE BOTTOM
+//SHAPE AND MESH
+const hotPlateGeo = new THREE.BoxGeometry(10, 6, 3);
+const hotPlateMesh = new THREE.MeshBasicMaterial({color: 0xE1E6E0});
+const hotPlate = new THREE.Mesh(hotPlateGeo, hotPlateMesh);
+scene.add(hotPlate);
+//ROTATION AND POSITIONING
+hotPlate.rotation.x = -1;
+hotPlate.position.x = 4;
+hotPlate.position.z = 20;
+
+//HOT PLATE TOP
+//SHAPE AND MESH
+const hotPlateTopGeo = new THREE.PlaneGeometry(10,7.5);
+const hotPlateTopMesh = new THREE.MeshBasicMaterial({color: 0xff0000});
+const hotPlateTop = new THREE.Mesh(hotPlateTopGeo, hotPlateTopMesh);
+scene.add(hotPlateTop);
+hotPlateTop.rotation.x = -1;
+hotPlateTop.position.x = 4;
+hotPlateTop.position.y = 1.79;
+hotPlateTop.position.z = 20;
+
+
+//TOPPINGS TABLE
+//SHAPE AND MESH
+const condimentsGeo = new THREE.BoxGeometry(10, 6, 3);
+const condimentsMesh = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const condiments = new THREE.Mesh(condimentsGeo, condimentsMesh);
+scene.add(condiments);
+condiments.rotation.x = -1;
+condiments.position.x = -6;
+condiments.position.z = 20;
 
 //BAGEL THAT FLOATS
 //TEXTURE
@@ -36,6 +69,7 @@ const bagelTexture = textureLoader.load('./img/doughnut.jpg')
 bagelTexture.wrapS = THREE.RepeatWrapping;
 bagelTexture.wrapT = THREE.RepeatWrapping;
 bagelTexture.repeat.set( 1.5, 1 );
+//SHAPE AND MESH
 const bagelGeo = new THREE.TorusGeometry(5, 3, 26, 26);
 const bagelMaterial = new THREE.MeshBasicMaterial({map : bagelTexture});
 const bagel = new THREE.Mesh(bagelGeo, bagelMaterial);
